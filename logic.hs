a2a77182794eb8c65f3f5989613f5273fd76433f
Gorelov VV <vyacheslav.gorelov93@gmail.com>
2014-12-21 00:42:51 +0300

    get/set field

---
+++ logic.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+type Field = [State]
+
+data State = Black | White | Empty
+	deriving (Show, Eq)
+
+count :: Int
+count = 8
+
+startField :: Field
+startField = replicate (count * count) Empty
+
+getState :: Field -> Int -> Int -> State
+getState field i j = field !! (i * count + j)
+
+setState :: Field -> State -> Int -> Int -> Field
+setState field state i j = take (i * count + j) field ++ [state] ++ drop (i * count + j + 1) field
+	
