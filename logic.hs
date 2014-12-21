9eaffe2584c0d1a59183ab1ba4634a90d18b4291
Gorelov VV <vyacheslav.gorelov93@gmail.com>
2014-12-21 22:34:34 +0300

    add nextStep

--- logic.hs
+++ logic.hs
@@ -4,13 +4,6 @@ import Data.List
 import System.IO.Unsafe
 import Checking
 import Types
-import Graphics.UI.WXCore
-import Graphics.UI.WX
-import Data.Array.IArray
-import Data.List.Split
-import Data.Maybe
-import Data.IORef
-import Control.Monad as Monad 
 
 startField :: Field
 startField = replicate (count * count) Empty
@@ -45,7 +38,7 @@ printField f = putStr $ getStrField f 1
 		getStrField field i
 			| i < count = show (take count field) ++ "\n" ++ getStrField (drop count field) (i+1)
 			| otherwise = show (take count field) ++ "\n"
-			
+
 -- поле - цвет ходящего - координаты хода
 moving :: Field -> State -> Position -> Field
 moving field state pos = if (state == Empty) then move field state pos (checkPosition field state pos) else field
@@ -55,10 +48,17 @@ f2 = setState f1 White (4,4)
 f3 = setState f2 Black (5,4)
 f4 = setState f3 White (5,5)
 
-startGame = f4
+fPath :: String
+fPath = "save.txt"
+
+startGame = fieldToIO f4
 
 writeGameToFileIO :: Field -> FilePath -> IO ()
 writeGameToFileIO arr filename = writeFile filename $ unwords $ map show $ arr
+
+intToPair k = ((k `div` count) + 1, (k `mod` count) + 1)
+
+nextStep k = fieldToIO $ moving (fieldFromIO (readGameFromFileIO fPath)) White $ intToPair k
 	
 readGameFromFileIO :: FilePath -> IO Field
 readGameFromFileIO filename = do
